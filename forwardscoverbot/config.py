fb71f45d0307ce591e2daa5d2b3438e01f9e1792
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2022-12-04 07:17:43 +0700

    Update config.py

--- forwardscoverbot/config.py
+++ forwardscoverbot/config.py
@@ -12,8 +12,8 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "5660988324:AAHb_O3YGm8otZGuE2O9R4v7i5WS0cYczYc")
     ADMINS = int(getenv("ADMINS", "5166575484"))
     DB_PATH = getenv("DB_PATH", "database/database.db")
-    GROUP = getenv("GROUP", "t.me/musikkugroup")
+    GROUP = getenv("GROUP", "t.me/arena4me")
     CHANNEL = getenv("CHANNEL", "t.me/DutabotID")
-    KODE = getenv("KODE", "https://github.com/kenkansaja/forwardmediabot")
+    
 config = Config()
 
